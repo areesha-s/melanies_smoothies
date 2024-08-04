300f3579badbada469c5388ed8fc75907a62342e
Areesha Shafiq <152386219+areesha-s@users.noreply.github.com>
2024-08-04 19:24:58 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,53 +1,42 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
-
+import requests
 
 # Write directly to the app
 
 # Title and Description
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
-st.write(
-    """ Choose the fruits you want in your custom Smoothie!""")
-
-
+st.write("Choose the fruits you want in your custom Smoothie!")
 
 name_on_order = st.text_input("Name on Smoothie:")
 st.write("The name on your Smoothie will be:", name_on_order)
 
-cnx=st.connection("snowflake")
+cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
-
+# st.dataframe(data=my_dataframe, use_container_width=True)
 
 ingredients_list = st.multiselect(
-    "Choose upto 5 ingredients: ",
+    "Choose up to 5 ingredients: ",
     my_dataframe,
-    max_selections= 5
-    
-   
+    max_selections=5
 )
+
 if ingredients_list:
+    ingredients_string = ''
+    for fruit_chosen in ingredients_list:
+        ingredients_string += fruit_chosen + ' '
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
+        fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+    # st.write(ingredients_string)    
 
- ingredients_string = '' 
- for fruit_chosen in ingredients_list:
-     ingredients_string += fruit_chosen+ ' '
- #st.write(ingredients_string)    
- 
- my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
-            values ('""" + ingredients_string + """','""" + name_on_order + """' )"""
- #st.write(my_insert_stmt)
-   
+    my_insert_stmt = """INSERT INTO smoothies.public.orders(ingredients, name_on_order)
+                        VALUES ('""" + ingredients_string + """', '""" + name_on_order + """')"""
+    # st.write(my_insert_stmt)
 
- time_to_insert = st.button('Submit Order')
+    time_to_insert = st.button('Submit Order')
 
- if time_to_insert:
-    session.sql(my_insert_stmt).collect()
-    st.success('Your Smoothie is ordered!', icon="✅")
-  import requests
-  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-  fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
-
-
- 
+    if time_to_insert:
+        session.sql(my_insert_stmt).collect()
+        st.success('Your Smoothie is ordered!', icon="✅")
